668b638db32722511b483ae3eab1d83ac12c4834
Anas Qadrei <anasqadrei@gmail.com>
2016-10-10 23:07:26 +1100

    adding Pod files to source control

---
+++ awtarika/Pods/Target Support Files/iOSLogEntries/iOSLogEntries-umbrella.h
@@ -0,0 +1,13 @@
+#import <UIKit/UIKit.h>
+
+#import "LEBackgroundThread.h"
+#import "lecore.h"
+#import "lelib.h"
+#import "LELog.h"
+#import "LeNetworkStatus.h"
+#import "LogFile.h"
+#import "LogFiles.h"
+
+FOUNDATION_EXPORT double iOSLogEntriesVersionNumber;
+FOUNDATION_EXPORT const unsigned char iOSLogEntriesVersionString[];
+

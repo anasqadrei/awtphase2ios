de00172ad123ece4022d4255a4b8fb90946f1465
Anas Qadrei <anasqadrei@gmail.com>
2016-10-10 23:54:37 +1100

    re-fixing development branch (copied from master)

--- awtarika/awtarika/Constants.swift
+++ awtarika/awtarika/Constants.swift
@@ -11,13 +11,14 @@ import Foundation
 struct Constants {
     
     struct URLs {
-        static let Host = "http://www.awtarika.com"
+        static let Host = "http://awtphase2dev.herokuapp.com"
+//        static let Host = "http://localhost:3000"
     }
     
     struct AdMob {
-        static let ApplicationID = "ca-app-pub-8300933998503689~3927627683"
-        static let SongScreenAdUnitID = "ca-app-pub-8300933998503689/5404360881"
-        static let PlayerScreenAdUnitID = "ca-app-pub-8300933998503689/6881094088"
+        static let ApplicationID = "ca-app-pub-8300933998503689~9095334088"
+        static let SongScreenAdUnitID = "ca-app-pub-8300933998503689/3048800489"
+        static let PlayerScreenAdUnitID = "ca-app-pub-8300933998503689/5943391284"
         static let TestDeviceAnasIPhone4S = "df54a0ff1ed59ea08ba3984e50f7825e"
     }
     
